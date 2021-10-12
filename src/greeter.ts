e1c8adc76898d09357019a21456250ded053a099
Wyseguys <wyseguys@gmail.com>
2021-10-11 17:16:49 -0700

    learnin\' me some modren wab developments and such

---
+++ src/greeter.ts
@@ -0,0 +1,26 @@
+class Student
+{
+    fullname: string;
+    constructor(
+        public firstname: string,
+        public middlename: string,
+        public lastname: string
+    ){
+        this.fullname = firstname + " " + middlename + " "+ lastname;
+    }
+}
+
+
+interface person {
+    firstname: string;
+    lastname: string
+}
+
+
+function greeter(person: Student) {
+    return "Hello, " + person.firstname + " " + person.middlename + " "  + person.lastname;
+  }
+   
+  let user = new Student("Mr Christer", "Lee", "Zwemke");
+   
+  document.body.textContent = greeter(user);
